4c73722fa03f9320781502012cd78f14b53a3702
Emily Yao <emilyweiyao@gmail.com>
2023-01-03 14:53:22 -0500

    top button

--- main.js
+++ main.js
@@ -28,14 +28,23 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-//progress scoller
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// Get the button:
+let mybutton = document.getElementById("myBtn");
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
