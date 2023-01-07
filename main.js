2923db7c8c4fbf2df9b608470b2810ea03876897
Emily Yao <yao.e@ufl.edu>
2023-01-06 22:35:14 -0500

    final button work

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function showSlides(n) {
 }
 
 // Get the button:
-let mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementById("Btn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
